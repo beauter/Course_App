f77000a0c2f5819173e0a7957eba72966e37f981
beauter <106946799+beauter@users.noreply.github.com>
2023-05-23 13:54:28 +0300

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL; // Replace with your Supabase URL
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY; // Replace with your Supabase public API key
+const supabaseUrl = 'https://educzwtlturtubfadfuw.supabase.co'
+const supabaseKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkdWN6d3RsdHVydHViZmFkZnV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ4MzM4NzgsImV4cCI6MjAwMDQwOTg3OH0.Gd0RuR50fFm0o7IRh14WTIM8VnPUHpPyHQm5nkQoEHc
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
