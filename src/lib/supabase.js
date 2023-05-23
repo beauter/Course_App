8749205349c17927300bd65fe257006a1d01d7f5
beauter <106946799+beauter@users.noreply.github.com>
2023-05-23 14:30:13 +0300

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://educzwtlturtubfadfuw.supabase.co'
-const supabaseKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkdWN6d3RsdHVydHViZmFkZnV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ4MzM4NzgsImV4cCI6MjAwMDQwOTg3OH0.Gd0RuR50fFm0o7IRh14WTIM8VnPUHpPyHQm5nkQoEHc
-
+const supabaseUrl ='https://educzwtlturtubfadfuw.supabase.co'
+const supabaseKey ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkdWN6d3RsdHVydHViZmFkZnV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ4MzM4NzgsImV4cCI6MjAwMDQwOTg3OH0.Gd0RuR50fFm0o7IRh14WTIM8VnPUHpPyHQm5nkQoEHc'
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
